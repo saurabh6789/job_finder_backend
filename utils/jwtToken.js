2a3e326b96e96237a96af007b78cd19ec6b2c7a7
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 14:29:42 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + 5 * 24 * 60 * 60 * 1000*10
     ),
     httpOnly: true, // Set httpOnly to true
+    secure: process.env.NODE_ENV === 'production',
+        sameSite: 'Strict'
   };
 
   res.status(statusCode).cookie("token", token, options).json({
